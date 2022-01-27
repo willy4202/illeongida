bb084d0144ce3ec9064993a0eb95bfd8c587272f
young970102 <60802735+young970102@users.noreply.github.com>
2022-01-27 18:45:00 +0900

    javas

---
+++ js/javas.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.addEventListener("touchstart", playVideo);
+    function playVideo() {
+        const video = document.getElementById("back-vid");
+        if (video.playing) {
+        } else {
+            video.play();
+        }
+    }
+});
